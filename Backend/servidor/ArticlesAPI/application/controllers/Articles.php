94df5f14acbb921ce8772f347f7650e8dbb9448c
Alexander Londoño Espejo <alexander.londono@secuencia24.com>
2017-03-25 22:44:57 -0500

    Agregando la logica de nuestro Contolador Articles

--- Backend/servidor/ArticlesAPI/application/controllers/Articles.php
+++ Backend/servidor/ArticlesAPI/application/controllers/Articles.php
@@ -15,22 +15,63 @@ class Articles extends REST_Controller {
     }
 
     public function index_get(){
-        echo 'Articles All';
+        $articles = $this->articles_model->get();
+        if(!is_null($articles)){
+            $this->response(array('response' => $articles), 200);
+        }else{
+            $this->response(array('error' => 'No hay articulos en la base de datos'), 404);
+        }
     }
 
     public function find_get($id){
-        echo 'Article # ' .$id;
+        if(!$id){
+            $this->reponse(null, 400);
+        }
+
+        $article = $this->articles_model->get($id);
+        if(!is_null($article)){
+            $this->response(array('response' => $article), 200);
+        }else{
+            $this->response(array('error' => 'Articulo no encontrado'), 404);
+        }
     }
 
     public function index_post(){
+        if(!$this->post('article')){
+            $this->reponse(null, 400);
+        }
 
+        $data = $this->articles_model->save($this->post('article'));
+        if(!is_null($data)){
+            $this->response(array('response' => $data), 200);
+        }else{
+            $this->response(array('error' => 'Error en el servidor'), 400);
+        }
     }
 
-    public function index_put(){
-        
+    public function index_put($id){
+        if(!$this->post('article') || !$id){
+            $this->response(null, 400);
+        }
+
+        $update = $this->articles_model->update($id, $this->post('artcile'));
+        if(!is_null($update)){
+            $this->response(array('response' => 'Articulo editado correctamente'), 200);
+        }else{
+            $this->response(array('error' => 'Error en el servidor'), 400);
+        }
     }
 
-    public function delete_put(){
-        
+    public function delete_put($id){
+        if(!$id){
+            $this->response(null, 400);
+        }
+
+        $delete = $this->articles_model->delete($id);
+        if(!is_null($delete)){
+             $this->response(array('response' => 'Articulo eliminado correctamente'), 200);
+        }else{
+            $this->response(array('error' => 'Error en el servidor'), 400);
+        }
     }
 }
